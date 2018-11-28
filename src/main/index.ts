cfbd9bf5eb49a77b9b4c61cacd205c7b384e7ff7
yahvz01 <yahvz01@gmail.com>
2018-11-28 21:29:46 +0900

    Build Configuration Fixed

--- src/main/index.ts
+++ src/main/index.ts
@@ -35,13 +35,17 @@ function mapOf<_TpK, _TpV>(...data : MapTuple<_TpK, _TpV>[]) : Map<_TpK, _TpV>{
     return HashMap.of(...data)
 }
 
+function tupleOf<_TpK, _TpV>(key : _TpK, value : _TpV) : MapTuple<_TpK, _TpV>{
+    return MapTuple.of(key, value)
+}
+
 
 export {
     Traversable, // traverable
     Seq, Set, Map,
     Gen, MapTuple, hashCode, deepCopy, // generic
     Vector, HashSet, HashMap, List,// immutable
-    listOf, seqOf, setOf, mapOf, // immutable Helper function
+    listOf, seqOf, setOf, mapOf, tupleOf,// immutable Helper function
     Buffer, MutableStack, BufferedStack, MutableQueue, BufferedQueue,  // mutable
     mutableQueueOf, mutableStackOf, // mutableHelper
     Optional
